601c783b1bad3ea92d58a884deeedc4b0fecafc8
Faisal Waris <faisalwaris@renesistech.com>
2022-03-06 14:17:59 +0500

    changed image type for internet images

--- app/src/main/java/com/example/aomatatask/util/AppConstant.kt
+++ app/src/main/java/com/example/aomatatask/util/AppConstant.kt
@@ -6,7 +6,7 @@ object AppConstant {
      */
     const val BASE_URL = "https://pixabay.com/"
     const val PIXABAY_KEY = "25970903-8587bcbe8d7347ef7a64f5ddf"
-    const val QUERY_IMAGE_QUERY = "galaxy"
+    const val QUERY_IMAGE_QUERY = "discovery"
     const val QUERY_IMAGE_TYPE = "photo"
 
 
